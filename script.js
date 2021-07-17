ab57c92968e42b01b077b09d545ac80ddd9c497c
Gabriel <gabrielmatuk14@gmail.com>
2021-07-17 00:35:43 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const changePageLinkedin = () => {
+    location.replace("https://br.linkedin.com/in/gabrielmatuk")
+}
+
+const changePageGithub = () => {
+    location.replace("https://github.com/gabrielmatuk")
+}
+
+//script mixuruco
